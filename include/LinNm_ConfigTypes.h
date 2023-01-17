0c99b170cdd006342cd6f8cf4e0b0a0203092dc8
drozycki <drozycki@student.agh.edu.pl>
2023-01-18 00:49:30 +0100

    Added config for LinNm

--- include/LinNm_ConfigTypes.h
+++ include/LinNm_ConfigTypes.h
@@ -2,10 +2,26 @@
 #define LINNM_CONFIGTYPES_H_
 
 typedef struct {
-    
+    const uint32 TimeoutTime;
+    const NetworkHandleType FrNmComMNetworkHandleRef;
+} LinNm_ChannelConfigType;
+
+typedef struct {
+    const LinNm_ChannelConfigType* LinNmChannels;
+    const boolean BusSyncEnabled;
+    const boolean ComCtrlEnabled;
+    const boolean ComUserDataSupport;
+    const boolean CoordinatorSyncSupport;
+    const boolean DevErrorDetect;
+    const boolean MainFunctionPeriod;
+    const boolean NodeDetectionEnabled;
+    const boolean NodeIdEnabled;
+    const boolean PassiveModeEnabled;
+    const boolean RemoteSleepIndicationEnabled;
+    const boolean StateChangeIndEnabled;
+    const boolean SyncPointEnabled;
+    const boolean UserDataEnabled;
+    const boolean VersionInfoApi;
 }LinNm_ConfigType;
-
-
-
 
 #endif LINNM_CONFIGTYPES_H_
